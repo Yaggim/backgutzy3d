80a30ab1b5e7ac711fdb0d48fecca7f5dd18b029
Yaggi <yaggim@gmail.com>
2024-06-17 03:02:00 -0300

    Se agrega api para registro con validaciones

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 import pool from '../config/database.js';
 
-// Función para crear un usuario
 export async function crearUsuario(req, res) {
     try {
         const { nombre, apellido, email, password } = req.body;
@@ -11,7 +10,18 @@ export async function crearUsuario(req, res) {
             [email]
         );
 
+        // Verifica si el email ya existe en la tabla usuario
+        const [existingClient] = await pool.query(
+            'SELECT * FROM cliente WHERE Email = ?',
+            [email]
+        );
+
+
         if (existingUser.length > 0) {
+            return res.status(400).json({ error: 'El correo electrónico ya está registrado' });
+        }
+
+        if (existingClient.length > 0) {
             return res.status(400).json({ error: 'El correo electrónico ya está registrado' });
         }
 
@@ -19,13 +29,19 @@ export async function crearUsuario(req, res) {
         const fechaRegistro = new Date().toISOString().slice(0, 19).replace('T', ' ');
         console.log(fechaRegistro);
 
+        // Inicia una transacción
+        await pool.query('START TRANSACTION');
+
         // Inserta el cliente usando el procedimiento almacenado AgregarCliente
-        const [clienteResult] = await pool.query(
+        await pool.query(
             'CALL AgregarCliente(?, ?, ?, ?)',
             [nombre, apellido, email, fechaRegistro]
         );
 
-        const idCliente = clienteResult.insertId;
+        // Obtén el último ID insertado
+        const [result] = await pool.query('SELECT LAST_INSERT_ID() AS insertId');
+        const idCliente = result[0].insertId;
+        console.log(idCliente);
 
         // Inserta el usuario asociado al cliente con rol USUARIO (asumimos Id_Rol = 2 para USUARIO)
         await pool.query(
@@ -33,9 +49,16 @@ export async function crearUsuario(req, res) {
             [password, email, 2, idCliente]
         );
 
+        // Confirma la transacción
+        await pool.query('COMMIT');
+
         res.status(201).json({ message: 'Registro exitoso' });
     } catch (error) {
         console.error('Error en el registro:', error);
+
+        // En caso de error, revierte la transacción
+        await pool.query('ROLLBACK');
+
         res.status(500).json({ error: 'Error en el registro' });
     }
 }
